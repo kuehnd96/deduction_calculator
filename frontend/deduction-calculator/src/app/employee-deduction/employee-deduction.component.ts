b858e471495db8b4ba8d1a76dcd651f0bb3648ee
David Kuehn <kuehnd96@gmail.com>
2021-12-27 16:54:47 -0600

    Implement deduction adding. ❄

--- frontend/deduction-calculator/src/app/employee-deduction/employee-deduction.component.ts
+++ frontend/deduction-calculator/src/app/employee-deduction/employee-deduction.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Dependent} from '../models/dependent';
 import { Employee} from '../models/employee';
 import { webClient } from '../backend/webClient';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-employee-deduction',
@@ -11,12 +12,34 @@ import { webClient } from '../backend/webClient';
 export class EmployeeDeductionComponent implements OnInit {
   firstName?: string;
   lastName?: string;
-  dependents?: Dependent[];
+  dependents: Dependent[] = new Array(4);
 
+  employeeForm = new FormGroup({
+    firstName: new FormControl(null, [Validators.required]),
+    lastName: new FormControl(null, [Validators.required]),
+  })
+
+  dependentForm = new FormGroup({
+    firstName: new FormControl(null, [Validators.required]),
+    lastName: new FormControl(null, [Validators.required]),
+  })
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addDependent() : void {
+    if (this.dependentForm.status == "VALID") {
+      let newDependent = new Dependent();
+      newDependent.firstName = this.dependentForm.controls['firstName'].value,
+      newDependent.lastName = this.dependentForm.controls['lastName'].value
+      
+      this.dependents.push(newDependent);
+    }
+  }
+
+  calculateDeduction() : void {
+
+  }
 }
